682eb3fe7c95e32c4537b5cf1694ec12356e1b36
HOANGPHI <phi.le@bnksolution.com>
2023-12-04 16:29:43 +0700

    active menu

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,40 +1,49 @@
-'use client'
-import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+"use client";
+import {
+  AppstoreOutlined,
+  MailOutlined
+} from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-import { useParams } from 'next/navigation'
 
-
-const items: MenuProps['items'] = [
+const items: MenuProps["items"] = [
   {
     label: <Link href={"/users"}>Manage Users</Link>,
-    key: 'users',
+    key: "users",
     icon: <MailOutlined />,
   },
   {
     label: <Link href={"/blogs"}>Manage Blogs</Link>,
-    key: 'blogs',
+    key: "blogs",
     icon: <AppstoreOutlined />,
   },
- 
 ];
 
 const Header: React.FC = () => {
-  const [current, setCurrent] = useState('users');
-  const pathname = usePathname()
-  const params = useParams();
-  console.log({pathname, params});
-  
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
+  const [current, setCurrent] = useState("users");
+  const pathname = usePathname();
+  const param = pathname.split("/");
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
     setCurrent(e.key);
   };
+  useEffect(() => {
+    console.log({ pathname, param });
+    setCurrent(param[1]);
+  }, [pathname, param]);
 
-  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
 };
 
 export default Header;
